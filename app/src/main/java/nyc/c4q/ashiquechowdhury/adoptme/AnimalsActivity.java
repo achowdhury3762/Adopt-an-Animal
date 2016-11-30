4f7c37f06749baa410bb7a57488b449e814efe70
achowdhury3762 <achowdhury3762@gmail.com>
2016-11-29 21:17:18 -0500

    started retro

--- app/src/main/java/nyc/c4q/ashiquechowdhury/adoptme/AnimalsActivity.java
+++ app/src/main/java/nyc/c4q/ashiquechowdhury/adoptme/AnimalsActivity.java
@@ -5,6 +5,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+
 /**
  * Created by ashiquechowdhury on 11/29/16.
  */
@@ -22,6 +25,15 @@ public class AnimalsActivity extends AppCompatActivity{
         mAnimalRecyclerView = (RecyclerView) findViewById(R.id.animal_recycler);
         mAnimalRecyclerView.setLayoutManager(new LinearLayoutManager(this));
 
+        callRetrofit();
+    }
+
+    private void callRetrofit() {
+        Retrofit retrofit = new Retrofit.Builder().addConverterFactory(GsonConverterFactory.create()).baseUrl("http://api.petfinder.com/").build();
+        PetFinderAPI petAPI = retrofit.create(PetFinderAPI.class);
+
 
     }
+
+
 }
---
+++ app/src/main/java/nyc/c4q/ashiquechowdhury/adoptme/model/PetFinder.java
@@ -0,0 +1,8 @@
+package nyc.c4q.ashiquechowdhury.adoptme.model;
+
+/**
+ * Created by ashiquechowdhury on 11/29/16.
+ */
+public class PetFinder {
+    PetFinder petfinder;
+}
