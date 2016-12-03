4ddf2f40594dcd7b54108dda801d746d73041700
achowdhury3762 <achowdhury3762@gmail.com>
2016-12-02 23:17:04 -0500

    Finishing Changes

--- app/src/main/java/nyc/c4q/ashiquechowdhury/adoptme/PetFinderAPI.java
+++ app/src/main/java/nyc/c4q/ashiquechowdhury/adoptme/PetFinderAPI.java
@@ -8,8 +8,7 @@ import retrofit2.http.Query;
 /**
  * Created by ashiquechowdhury on 11/29/16.
  */
-//baseurl - http://api.petfinder.com/
-//?key=591201d638c591f60b82a65aaed3bffa&location=11377&format=json
+
 public interface PetFinderAPI {
     @GET("pet.find")
     Call<RealPetfinder> getPetsByZipCode(@Query("key") String apiKey, @Query("format") String formatType, @Query("location") String zipCode);
